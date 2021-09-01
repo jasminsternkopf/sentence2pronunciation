71f17dc22fa0467afe7fae98eac4ce56fde11302
Jasmin Sternkopf <jasmin_sternkopf@web.de>
2021-09-02 01:26:16 +0200

    first methods, not ready yet

--- src/sentence2pronunciation/sentence2pronunciation.py
+++ src/sentence2pronunciation/sentence2pronunciation.py
@@ -1,4 +1,5 @@
-from typing import Dict, Set, Tuple
+import re
+from typing import Dict, List, Set, Tuple
 
 Pronun = Tuple[str, ...]
 Pronun_Dict = Dict[str, Pronun]
@@ -9,4 +10,33 @@ def sentence2pronunciaton(sentence: str, dict: Pronun_Dict, trim_symb: Set[str],
 
 
 def word2pronunciation(word: str, dict: Pronun_Dict, trim_symb: Set[str], split_on_hyphen: bool) -> Pronun:
-  pass
+  trim_beginning, act_word, trim_end = trim_word(word, trim_symb)
+  pronuns = [(trim_beginning)]
+  if split_on_hyphen:
+    splitted_words = act_word.split("-")
+    for index, single_word in enumerate(splitted_words):
+      pronuns.append(get_pronun(single_word, dict))
+      if index != len(splitted_words) - 1:
+        pronuns.append(("-"))
+  else:
+    pronuns.append(get_pronun(act_word, dict))
+  pronuns.append((trim_end))
+  complete_pronun = pronunlist_to_pronun(pronuns)
+  return complete_pronun  # - einfügen
+
+
+def trim_word(word: str, trim_symb: Set[str]) -> Tuple[str, str, str]:
+  trim_symb_single_str = "".join(str(symb) for symb in trim_symb)
+  trim = re.compile(rf"[{trim_symb_single_str}]*")
+  beginning = trim.match()
+  # ...
+
+
+def get_pronun(word: str, dict: Pronun_Dict, replace_unknown_with) -> Pronun:
+  if word in dict.keys():
+    return dict[word]
+  return replace_unknown_with * len(word)
+
+
+def pronunlist_to_pronun(pronunlist: List[Pronun]) -> Pronun:
+  return tuple(pronunlist)
