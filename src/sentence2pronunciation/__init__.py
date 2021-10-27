4533323befcae5ff5aab072451e605a520bdcf08
Stefan Taubert <stefan.taubert@posteo.de>
2021-10-27 15:43:20 +0200

    updated init

--- src/sentence2pronunciation/__init__.py
+++ src/sentence2pronunciation/__init__.py
@@ -3,3 +3,4 @@ from sentence2pronunciation.core import (
     sentence2pronunciation_cached, sentences2pronunciations_from_cache_mp,
     word2pronunciation, word2pronunciation_cached)
 from sentence2pronunciation.types import Pronunciation, Symbol
+from sentence2pronunciation.lookup_cache import LookupCache, get_empty_cache
