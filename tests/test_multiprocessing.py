834185c3cbde9c3e401d5b7c4203681cc02191ec
Stefan Taubert <stefan.taubert@posteo.de>
2021-11-08 16:19:25 +0100

    added stress test

--- tests/test_multiprocessing.py
+++ tests/test_multiprocessing.py
@@ -1,3 +1,9 @@
+import random
+import string
+from logging import getLogger
+from multiprocessing import Pool
+from typing import Tuple
+
 from sentence2pronunciation.multiprocessing import (
     prepare_cache_mp, sentences2pronunciations_from_cache_mp)
 from sentence2pronunciation.types import Pronunciation
@@ -63,3 +69,88 @@ def test_sentences2pronunciations_from_cache_mp():
                                   'A', 'X', ',', ' ', 'T', 'E', 'S', 'T', 'X', '.')
   assert result[sentences[1]] == ('T', 'H', 'I', 'S', 'X', ' ', 'I', 'S', 'X', ' ',
                                   'another', ' ', 'T', 'E', 'S', 'T', 'X', '.')
+
+
+def random_string_generator(str_size: int, allowed_chars: str):
+  return ''.join(random.choice(allowed_chars) for x in range(str_size))
+
+
+chars = string.ascii_letters + string.punctuation
+
+
+def get_random_sentence(words_count: int) -> Pronunciation:
+  words = []
+  for _ in range(words_count):
+    words.append(random_string_generator(random.randint(3, 10), chars))
+  return tuple(' '.join(words))
+
+
+def mp_get_random_sentence(i: int) -> Pronunciation:
+  return get_random_sentence(random.randint(3, 20))
+
+
+def test_prepare_cache_mp__stress_test():
+  n_jobs = 16
+  maxtasksperchild = None
+  chunksize = 10000
+  utterances_count = 1000000
+  # utterances_count = 1000
+  random.seed(1234)
+
+  logger = getLogger(__name__)
+  logger.info("Generating test sentences...")
+  with Pool(processes=n_jobs, maxtasksperchild=maxtasksperchild) as pool:
+    sentences = set(pool.map(mp_get_random_sentence,
+                             range(utterances_count), chunksize=chunksize))
+  logger.info("Done generating")
+
+  cache = prepare_cache_mp(
+    sentences=sentences,
+    annotation_split_symbol="/",
+    consider_annotation=True,
+    get_pronunciation=get_pronunciation_added_X,
+    ignore_case=True,
+    split_on_hyphen=True,
+    trim_symbols=set(string.punctuation),
+    chunksize=chunksize,
+    maxtasksperchild=maxtasksperchild,
+    n_jobs=n_jobs,
+  )
+
+  assert len(cache) > 0
+
+
+def get_random_word(i: int) -> Pronunciation:
+  return tuple(random_string_generator(random.randint(3, 10), chars))
+
+
+def test_sentences2pronunciations_from_cache_mp__stress_test():
+  n_jobs = 16
+  maxtasksperchild = None
+  chunksize = 10000
+  utterances_count = 1000000
+  # utterances_count = 1000
+  random.seed(1234)
+
+  logger = getLogger(__name__)
+  logger.info("Generating test words...")
+  with Pool(processes=n_jobs, maxtasksperchild=maxtasksperchild) as pool:
+    words = set(pool.map(get_random_word,
+                         range(utterances_count), chunksize=chunksize))
+  logger.info("Done generating")
+
+  cache = {word: word for word in words}
+  sentences = set(words)
+
+  result = sentences2pronunciations_from_cache_mp(
+    sentences=sentences,
+    annotation_split_symbol="/",
+    consider_annotation=True,
+    ignore_case=False,
+    cache=cache,
+    n_jobs=n_jobs,
+    chunksize=chunksize,
+    maxtasksperchild=maxtasksperchild,
+  )
+
+  assert len(result) > 0
